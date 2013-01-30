2da67639d4c7b3dbc0ff06438098d83aba5b35bf
David Knight <dknight@weblinc.com>
2013-01-30 02:49:54 -0500

    Major rework of media query parsing. Incorrectly formatted queries will have a more difficult time presenting false positives. Removal of dependencies. Build is targeted specifically as a polyfill. Core file name has changed from media.js to media.match.js to avoid confusion that project deals with video or images specifically and matches GitHub project name.

--- media.extension.js
+++
@@ -1,7 +0,0 @@
-/* Extension to Media - Adds new media features. Authors & copyright (c) 2012: WebLinc, David Knight. */
-
-// Added device-pixel-ratio even though it's covered by resolution #dppx
-window.Media.features["device-pixel-ratio"]  = window.Media.resolution;
-
-// Modernizr.touch
-window.Media.features["touch-enabled"]  = Number(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch || 0);
